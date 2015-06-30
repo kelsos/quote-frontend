e3a78cf5b0d4cc81ce5c2b8ce84c68111fcfa080
Konstantinos Paparas <kelsos86@gmail.com>
2015-07-01 00:24:19 +0300

    Adds placeholder for dialog

---
+++ app/scripts/controllers/adddialog.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quoteFrontendApp.controller:AdddialogCtrl
+ * @description
+ * # AdddialogCtrl
+ * Controller of the quoteFrontendApp
+ */
+angular.module('quoteApp')
+  .controller('AddDialogCtrl', function ($scope) {
+  });
--- app/scripts/controllers/quote.js
+++ app/scripts/controllers/quote.js
@@ -8,10 +8,20 @@
  * Controller of the quoteApp
  */
 angular.module('quoteApp')
-  .controller('QuoteCtrl', function ($scope, Restangular) {
+  .controller('QuoteCtrl', function ($scope, Restangular, $mdDialog) {
 
     var quotes = Restangular.all('quote');
     quotes.getList().then(function (results) {
       $scope.quotes = results;
     });
+
+    $scope.openDialog = function($event) {
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        parent: parentEl,
+        targetEvent: $event,
+        templateUrl: 'views/adddialog.html',
+        controller: 'AddDialogCtrl'
+      })
+    }
   });
